a6acd21d05015be2220fab1a3c7aa749dc5fcad4
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-07-02 10:46:02 +0700

    update server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+const PORT = 6969;
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./app/graphql/schema/");
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.json());
+app.use(
+    "/graphql",
+    graphqlHTTP({
+
+    })
+);
+
+app.listen(PORT, ()=>{
+    console.log("Server running")
+});
